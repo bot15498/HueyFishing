fe387cdc448cefd0097d31ef0c2acb350ea7045b
skyler kim <konata.izumi20@gmail.com>
2026-03-16 23:08:33 -0400

    Add some upgrades and add timeout to line

---
+++ HueyFishing/Assets/Scripts/Catching/DamageLine.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class DamageLine : MonoBehaviour
+{
+    public int playerDamageAmount = 10;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- HueyFishing/Assets/Scripts/Catching/FishCatchbar.cs
+++ HueyFishing/Assets/Scripts/Catching/FishCatchbar.cs
@@ -5,6 +5,7 @@ public class FishCatchbar : MonoBehaviour
 {
     public int currCatchBar = 0;
     public int maxCatchBar = 5;
+    public FishManager fishManager;
 
     void Start()
     {
@@ -16,13 +17,21 @@ public class FishCatchbar : MonoBehaviour
 
     }
 
-    public void IncreaseCatchBar(int value = 1)
+    public void IncreaseCatchBar(int value = 1, bool isFromCircle=true)
     {
         currCatchBar += value;
+        if (!isFromCircle && currCatchBar >= maxCatchBar)
+        {
+            // Need to do a circle around a fish for the final catch, even if bar is full
+            // Set bar back to 1 minus
+            currCatchBar = maxCatchBar - 1;
+        }
+
         if (currCatchBar >= maxCatchBar)
         {
             // you win!
             Debug.Log("Caught fish");
+            fishManager.currentFish.Remove(this);
             Destroy(gameObject);
         }
     }
