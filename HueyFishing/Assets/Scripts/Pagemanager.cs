61f536915b889faef74f3cede1608eaba598b726
AAYFlower <44514581+AAYFlower@users.noreply.github.com>
2026-03-16 19:59:00 -0700

    fas

--- HueyFishing/Assets/Scripts/Pagemanager.cs
+++ HueyFishing/Assets/Scripts/Pagemanager.cs
@@ -2,15 +2,46 @@ using UnityEngine;
 
 public class Pagemanager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [Header("Pages in order")]
+    public GameObject[] pages;
+    public GameObject nextPageButton;
+    public GameObject prevPageButton;
+    private int currentPage = 0;
+
     void Start()
     {
-        
+        ShowPage(currentPage);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void NextPage()
     {
-        
+        if (pages.Length == 0) return;
+
+        currentPage++;
+
+        if (currentPage >= pages.Length)
+            currentPage = pages.Length - 1; // stops at last page
+
+        ShowPage(currentPage);
+    }
+
+    public void PreviousPage()
+    {
+        if (pages.Length == 0) return;
+
+        currentPage--;
+
+        if (currentPage < 0)
+            currentPage = 0; // stops at first page
+
+        ShowPage(currentPage);
+    }
+
+    void ShowPage(int pageIndex)
+    {
+        for (int i = 0; i < pages.Length; i++)
+        {
+            pages[i].SetActive(i == pageIndex);
+        }
     }
 }
